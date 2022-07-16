d1f63f478290bf96830aaa92ee7b842405cd5643
soon-h <ksh8895420@gmail.com>
2022-07-16 17:05:56 +0900

    first commit

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,5 +1,6 @@
 def insertion_sort(array):
     for point in range(1,len(array)):
-        for compare in range(point, 0 , -1):
-            if array[compare - 1] > array[compare]:
-                array[compare - 1], array[compare] = array[compare], array[compare - 1]
+        i = point
+        while i > 0 and array[i - 1] > array[i]:
+            array[i - 1], array[i] = array[i], array[i - 1]
+            i -= 1
